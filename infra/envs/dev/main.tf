99e38c26bbdc85b1b746c181d0eb4bdab49d8f27
DHINESHKUMARGANESAN <dhinesh-kumar.ganesan.ext@siemens-energy.com>
2026-04-06 14:21:06 +0200

    Enable Copilot CLI authentication for Terraform scaffolding in CI

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -51,8 +51,7 @@ resource "azurerm_storage_account" "web" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
-
-  enable_https_traffic_only = true
+  https_traffic_only_enabled = true
   min_tls_version           = "TLS1_2"
 
   static_website {
